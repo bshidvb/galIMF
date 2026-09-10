d5be1a6aa00e74294992d2a4f755d02744ddb2b2
Adriana Zacharova <521129@mail.muni.cz>
2026-09-10 10:43:38 +0200

    Changed the Kroupa IMF to canonical (from bottom-heavy)

--- IMFs/Kroupa_IMF.py
+++ IMFs/Kroupa_IMF.py
@@ -1,19 +1,18 @@
 from scipy.integrate import quad
 
-alpha3 = 1.8
+alpha3 = 2.3
 
 def custom_imf_unnormalized(mass):  # there is no time dependence for Kroupa IMF
     if mass < 0.08:
         return 0
     elif mass < 0.5:
-        return 0.6*mass**(-3)
+        return 2*mass**(-1.3)
     elif mass < 1:
         return mass**(-2.3)
     elif mass < 150:
-        return mass**(-2.3)
+        return mass**(-alpha3)
     else:
         return 0
-
 def mass_function(mass):
     return custom_imf_unnormalized(mass) * mass
 
@@ -23,10 +22,10 @@ def custom_imf(mass, time=0):  # normalized to a population with mass = 1 Msun
     if mass < 0.08:
         return 0
     elif mass < 0.5:
-        return 0.6*mass**(-3)/integrated_mass
+        return 2*mass**(-1.3)/integrated_mass
     elif mass < 1:
         return mass**(-2.3)/integrated_mass
     elif mass < 150:
-        return mass**(-2.3)/integrated_mass
+        return mass**(-alpha3)/integrated_mass
     else:
         return 0
