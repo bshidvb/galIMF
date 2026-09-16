a3795c4a2359978fc7d619cf1c51e741ab4764ce
Adriana Zacharova <521129@mail.muni.cz>
2026-09-16 15:33:19 +0200

    changed parameters/file names

--- galaxy_evolution_nitrogen.py
+++ galaxy_evolution_nitrogen.py
@@ -2,16 +2,14 @@ import galevo_nitrogen as galevo
 
     # For gas_mass_dependent SFH model, we need to provide the gas mass evolution.
     # This is an example of how to generate a gas mass evolution.
-Log_SFR_list = [1.0]   # 20 M_sun/yr (Tacchella et al. 2023)
-SFEN_list = [100]       # 70 ± 40 Myr (Tacchella et al. 2023)
+Log_SFR_list = [0.2]   # 20 M_sun/yr (Tacchella et al. 2023)
+SFEN_list = [15]       # 70 ± 40 Myr (Tacchella et al. 2023)
 STF_list = [0.04]
-SFE_list = [0.008]
-tau_infall_list = [0.0065]
-# Log_SFR = 1.3 # for 20 M_sun/yr - real value
+SFE_list = [0.006]
+tau_infall_list = [0.0085]
 location = 10
 skewness = 0
 sfr_tail = 0
-# SFEN = 5 # for 66 Myr
 
 for SFEN in SFEN_list:
     for Log_SFR in Log_SFR_list:
@@ -19,17 +17,34 @@ for SFEN in SFEN_list:
             for SFE in SFE_list:
                 for tau_infall in tau_infall_list:
                     galevo.generate_SFH('flat',  Log_SFR, SFEN, sfr_tail, skewness, location)
-                    galevo.galaxy_evol(imf='igimf', STF=STF, SFEN=SFEN, Z_0=1e-6, solar_mass_component="Asplund2009_mass",
+                    galevo.galaxy_evol(imf='Kroupa', STF=STF, SFEN=SFEN, Z_0=1e-6, solar_mass_component="Asplund2009_mass",
                                     str_yield_table='Nomoto_sup_agb', IMF_name='Kroupa', steller_mass_upper_bound=150, #try 100
                                     time_resolution_in_Myr=1, mass_boundary_observe_low=1.5, mass_boundary_observe_up=8,
                                     SFH_model='gas_mass_dependent', SFE=SFE, SNIa_ON=True, SNIa_yield_table='Iwamoto1999',
                                     solar_abu_table='Asplund2009',
-                                    high_time_resolution=None, plot_show=None, plot_save=True, outflow=110, check_igimf=None, tau_infalle9=tau_infall)
+                                    high_time_resolution=None, plot_show=None, plot_save=True, outflow=120, check_igimf=None, tau_infalle9=tau_infall)
 
-# Bekki input
-# galevo.galaxy_evol(imf='Kroupa', STF=0.5, SFEN=SFEN, Z_0=0.015*1e-6, solar_mass_component="Asplund2009_mass",
-#                 str_yield_table='Kobayashi06', IMF_name='Kroupa', steller_mass_upper_bound=120,
-#                 time_resolution_in_Myr=1, mass_boundary_observe_low=1.5, mass_boundary_observe_up=8,
-#                 SFH_model='provided', SFE=0.04, SNIa_ON=True, SNIa_yield_table='Iwamoto1999',
-#                 solar_abu_table='Asplund2009',
-#                 high_time_resolution=None, plot_show=None, plot_save=None, outflow=100, check_igimf=None)
+# variable imf for n13 model 
+# Log_SFR_list = [1.0]   # 20 M_sun/yr (Tacchella et al. 2023)
+# SFEN_list = [100]       # 70 ± 40 Myr (Tacchella et al. 2023)
+# STF_list = [0.04]
+# SFE_list = [0.008]
+# tau_infall_list = [0.0065]
+# # Log_SFR = 1.3 # for 20 M_sun/yr - real value
+# location = 10
+# skewness = 0
+# sfr_tail = 0
+# # SFEN = 5 # for 66 Myr
+
+# for SFEN in SFEN_list:
+#     for Log_SFR in Log_SFR_list:
+#         for STF in STF_list:
+#             for SFE in SFE_list:
+#                 for tau_infall in tau_infall_list:
+#                     galevo.generate_SFH('flat',  Log_SFR, SFEN, sfr_tail, skewness, location)
+#                     galevo.galaxy_evol(imf='igimf', STF=STF, SFEN=SFEN, Z_0=1e-6, solar_mass_component="Asplund2009_mass",
+#                                     str_yield_table='Nomoto_sup_agb', IMF_name='Kroupa', steller_mass_upper_bound=150, #try 100
+#                                     time_resolution_in_Myr=1, mass_boundary_observe_low=1.5, mass_boundary_observe_up=8,
+#                                     SFH_model='gas_mass_dependent', SFE=SFE, SNIa_ON=True, SNIa_yield_table='Iwamoto1999',
+#                                     solar_abu_table='Asplund2009',
+#                                     high_time_resolution=None, plot_show=None, plot_save=True, outflow=110, check_igimf=None, tau_infalle9=tau_infall)
